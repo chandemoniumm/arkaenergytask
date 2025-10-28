b0d48d4c200f100866ea8b041e405935b0ddb90f
Chandemonium <garimella.chandrasekhar30@gmail.com>
2025-10-28 23:11:24 +0530

    Task 2 (Django) – Build a Django project with an app named 'students' having model Student (name, age, email) and API endpoint /api/students/ supporting GET and POST methods and Bonus Task – Add endpoint /api/students/adults/ returning students aged > 18.

---
+++ TASK2/students/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import student_list, adult_students
+
+urlpatterns = [
+    path('api/students/', student_list, name='student-list'),
+    path('api/students/adults/', adult_students, name='adult-student-list'),
+]
+
